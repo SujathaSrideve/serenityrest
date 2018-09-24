c83dd80dbcd021919477c082f1608618f36afc68
SujathaSrideve <41382714+SujathaSrideve@users.noreply.github.com>
2018-09-24 17:32:19 +0300

    Add files via upload
    file cleaned

--- src/test/java/serenityrest/serenity/DemoBlazeApiActions.java
+++ src/test/java/serenityrest/serenity/DemoBlazeApiActions.java
@@ -11,45 +11,31 @@ import static org.hamcrest.Matchers.equalTo;
 public class DemoBlazeApiActions {
     @Step
     public void requestAddToCart(String value){
-        //Map<String, String> params = new HashMap<>();
-        //params.put("id", cityId);
-        SerenityRest.given().contentType("application/json")
-                .and().body(value)
-                .when().post("https://api.demoblaze.com/addtocart");//http://localhost:3000/addtocart
+        SerenityRest.given().contentType("application/json").and().body(value).when().post("https://api.demoblaze.com/addtocart");//http://localhost:3000/addtocart
     }
 
     @Step
     public void requestDeleteItemFromCart(String value){
-        //Map<String, String> params = new HashMap<>();
-        //params.put("id", cityId);
-        SerenityRest.given().contentType("application/json")
-                .and().body(value)
-                .when().post("https://api.demoblaze.com/deleteitem");//"http://localhost:3000/deleteitem");
+        SerenityRest.given().contentType("application/json").and().body(value).when().post("https://api.demoblaze.com/deleteitem");//"http://localhost:3000/deleteitem");
     }
 
     @Step
     public void verifyAddToCartStatusCode(int responseCode){
-        SerenityRest.then().log().all().
-                statusCode(responseCode);//.and().body().equals("{}");
+        SerenityRest.then().log().all().statusCode(responseCode);
     }
 
     @Step
     public void verifyDeleteItemStatusCode(int responseCode){
-        SerenityRest.then().log().all().
-                statusCode(responseCode);//.and().body().equals("{}");
+        SerenityRest.then().log().all().statusCode(responseCode);
     }
 
     @Step
     public String verifyAddToCartResponse(){
        return SerenityRest.then().extract().body().asString();
-
     }
 
     @Step
     public String verifyDeleteItemResponse(){
-
        return SerenityRest.then().extract().body().jsonPath().get("errorMessage");
-
-
     }
 }
